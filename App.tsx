98c779bf1e5c7fba1af4a50d91595d31308397b3
Siso Ngqolosi <sisoalbert@yahoo.com>
2024-02-10 14:29:29 +0200

    Adding pressable components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./components/header";
+import Body from "./components/body";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Body />
       <StatusBar style="auto" />
     </View>
   );
